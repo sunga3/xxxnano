08c02825b614daebd4a969a476ab319799c91f75
sunga3 <121183110+sunga3@users.noreply.github.com>
2022-12-28 21:45:22 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const showContainer = document.querySelectorAll(".show-replies");
+
+showContainer.forEach((btn) => 
+btn.addEventListener("click", (e) =>{
+    let parentContainer = e.target.closest(".comment-container");
+    let _id = parentContainer.id;
+   if (_id) {
+    let childrenContainer = parentContainer.querySelectorAll(
+        `[dataset=${_id}]`
+        );
+    childrenContainer.forEach((child) => child.classList.toggle("opened"));
+   }
+    })
+);
+
+
+
+
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var container = document.querySelector(".container");
@@ -7,3 +25,4 @@ menuIcon.onclick =  function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container")
 }
+
