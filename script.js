5fad34129016b9c7df8f6d950c9450cffeee89a3
sunga3 <121183110+sunga3@users.noreply.github.com>
2023-01-04 00:51:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const showContainer = document.querySelectorAll(".show-replies");
-
-showContainer.forEach((btn) => 
-btn.addEventListener("click", (e) =>{
-    let parentContainer = e.target.closest(".comment-container");
-    let _id = parentContainer.id;
-   if (_id) {
-    let childrenContainer = parentContainer.querySelectorAll(
-        `[dataset=${_id}]`
-        );
-    childrenContainer.forEach((child) => child.classList.toggle("opened"));
-   }
-    })
-);
-
 
 
 
