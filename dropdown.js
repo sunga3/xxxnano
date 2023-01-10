0c20e75d6fbb0c30dac3196061040439b7539c6f
sunga3 <121183110+sunga3@users.noreply.github.com>
2023-01-11 02:15:14 +0800

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -8,12 +8,38 @@ function toggleMenu(){
 var newMenu2 = document.getElementById("newMenu2");
 
 
-function toggleMenu(){
-    newMenu2.classList.toggle("open-menu2");
+function toggleMenu2(){
+    newMenu2.classList.toggle("open-menu");
     
 }
+var newMenu3 = document.getElementById("newMenu3");
 
 
+function toggleMenu3(){
+    newMenu3.classList.toggle("open-menu");
+    
+}
+var newMenu4 = document.getElementById("newMenu4");
+
+
+function toggleMenu4(){
+    newMenu4.classList.toggle("open-menu");
+    
+}
+var newMenu5 = document.getElementById("newMenu5");
+
+
+function toggleMenu5(){
+    newMenu5.classList.toggle("open-menu");
+    
+}
+var newMenu6 = document.getElementById("newMenu6");
+
+
+function toggleMenu6(){
+    newMenu6.classList.toggle("open-menu");
+    
+}
 
 
 
